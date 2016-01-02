0689b5095f623052573f77109a80af72ed24affa
thesky <thesky@DESKTOP-8ECED36>
2016-01-03 04:05:49 +0900

    ..

--- kiwoom/src/BeautifulSoup.py
+++ kiwoom/src/BeautifulSoup.py
@@ -4,18 +4,13 @@ import urllib
 import requests
 
 class bts:
-# 가져올 URL
+
 # html = requests.get("http://finance.daum.net/quote/all.daum?type=U&stype=Q")
 # html = requests.get("http://finance.daum.net/quote/all.daum?type=U&stype=P") #코스피
 
     def IframeUrl(self):
-#         html = requests.get("http://finance.daum.net/item/hhmm.daum?code=069110")
-#         html = requests.get(urladdress)
-#         bt = BeautifulSoup(html.text,"lxml")
-#         forchild = bt.find('iframe')
-#         iframe_src=forchild.attrs['src']
-#         iframe_src='http://finance.daum.net'+iframe_src
-        code='069110'   #하이비젼
+
+        code='069110'   #코스온
 #         code='021080'  #에이티넘
         self.total=0
         self.end=True
@@ -23,7 +18,6 @@ class bts:
         while self.end:
             
             iframe_src='http://finance.daum.net/item/quote_hhmm_sub.daum?page='+str(self.page)+'&code='+str(code)
-            print('you requested src is : %s'%iframe_src)
             self.iframeParse(iframe_src)
             self.page+=1
         print("총 갯수 :"+str(self.total))
@@ -31,8 +25,9 @@ class bts:
         
     def iframeParse(self,IframeAddress):
         self.iframe_content=BeautifulSoup(requests.get(IframeAddress).text,"lxml")
-        self.printTimeResult()
-        self.printPercentResult()
+#         self.printTimeResult()
+#         self.printPercentResult()
+        self.mergeTimeAndPercent()
 
     def printTimeResult(self):
         eachTime = self.iframe_content.findAll("td",class_="datetime2")
@@ -46,6 +41,23 @@ class bts:
                 self.end=False
                 break
             self.total+=1
+            
+    def mergeTimeAndPercent(self):
+        eachTime = self.iframe_content.findAll("td",class_="datetime2")
+#         eachPercent = self.iframe_content.findAll("td",class_="num cUp")
+#         eachPercent = eachTime.next_sibling().next_sibling().next_sibling()
+        
+        self.dictt={}
+        for i in range(0,eachTime.__len__()):
+            print('eachTime : '+str(eachTime[i].contents[0]))
+            print('eachPercent : '+str(eachTime[i].next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.contents[0]))
+            if eachTime[i].contents[0]=='09:00':          #실전에서 수정필요
+                self.end=False
+                break
+#             self.dictt[eachTime[i]]=eachPercent[i]
+            self.total+=1
+#         print('#######'+str(self.dictt))
+            
 #     def printPercentResult(self):
 #         eachPercent = self.iframe_content.findAll("td",class_="num cUp")
 #         for a in range(self.total):
@@ -53,11 +65,8 @@ class bts:
 #             self.total+=1
                 
 
-
-
-
 if __name__ == "__main__":
     bttest = bts()
-    stockcode='069110'
+    
     bttest.IframeUrl()
     
