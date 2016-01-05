03b7fca62fa825dc5ec5fdeb8b8b7aed0c02d362
thesky <thesky@DESKTOP-8ECED36>
2016-01-05 20:53:30 +0900

    .

--- kiwoom/src/ExcelMake.py
+++ kiwoom/src/ExcelMake.py
@@ -60,7 +60,7 @@ class ExcelCode:
                 c+=1
             print(str(d-1)+'라인 퍼센테이지 입력 ('+str(time.time()-start_time)+')')
             print('d : '+str(d)+' c : '+str(c)) 
-            c =3 #한종목 파시앋하면 다시초기화
+            c =3 #한종목 파싱 다 하면 다시초기화
             d+=1
         print('1100라인  making '+str(time.time()-start_time))
     def excelVisible(self):
