b8740175746cdb01e1f521b0bd12d4d3331951b7
kiwoom <kiwoom@kiwoom-PC>
2016-01-05 23:14:32 +0900

    .,

--- kiwoom/src/ExcelMake.py
+++ kiwoom/src/ExcelMake.py
@@ -54,15 +54,29 @@ class ExcelCode:
         c =3
         d =2
         self.excelVisible()
-        for b in range(3,1100):
+        for howmanytimes in range(3,self.codelist.__len__()):
             for a in codeCurrList:
                 self.ws.Cells(d,c).Value = a
                 c+=1
             print(str(d-1)+'라인 퍼센테이지 입력 ('+str(time.time()-start_time)+')')
             print('d : '+str(d)+' c : '+str(c)) 
-            c =3 #한종목 파시앋하면 다시초기화
+            c =3 #한종목 파싱 다 하면 다시초기화
             d+=1
         print('1100라인  making '+str(time.time()-start_time))
+        
+    def WriteTimePerDict(self):
+        start_time=time.time()
+        bt = bts.mbts()
+        self.excelVisible()
+
+        for StckCode in self.codelist:
+            bt.IframeUrlWithCode(StckCode)
+            TimerPerDict = bt.getTimePerDic()
+            
+            '''
+            #Stock코드에 따라서 퍼센테이지 넣는코드 작성하시오.
+            '''
+            
     def excelVisible(self):
         self.excel.Visible = True
         
--- kiwoom/src/bts.py
+++ kiwoom/src/bts.py
@@ -10,14 +10,17 @@ class mbts:
 # html = requests.get("http://finance.daum.net/quote/all.daum?type=U&stype=P") #코스피
 
     def IframeUrl(self):
-
+        '''
+        this is  example
+        '''
         code='069110'   #코스온
 #         code='021080'  #에이티넘
         self.total=0
         self.end=True
         self.page=1
         self.eachPercent=[]
-        
+        self.timePerDic={}
+        start_time=time.time()
         while self.end:
             
             iframe_src='http://finance.daum.net/item/quote_hhmm_sub.daum?page='+str(self.page)+'&code='+str(code)
@@ -39,6 +42,7 @@ class mbts:
         self.end=True
         self.page=1
         self.eachPercent=[]
+        self.timePerDic={}
         start_time=time.time()
         while self.end:
             
@@ -46,11 +50,6 @@ class mbts:
             self.iframeParse(iframe_src)
             self.page+=1
             
-#         print("총 갯수 :"+str(self.total))
-#         print("총 페이지 :"+str(self.page))
-#         
-#         print("show PercentList : "+str(self.eachPercent))
-#         print("show PercentTotal "+str(self.eachPercent.__len__()))
         end_time=time.time()
         print('1종목  파싱완료 ('+str(end_time-start_time)+')')
 
@@ -61,13 +60,15 @@ class mbts:
         
         
         for i in range(0,eachTime.__len__()):
-
+            
             self.eachPercent.append(eachTime[i].next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
+            self.timePerDic[eachTime[i].contents[0]]=eachTime[i].next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.contents[0]
+            
             if eachTime[i].contents[0]=='09:00':          #실전에서 수정필요
                 self.end=False
                 break
             self.total+=1
-#         self.showEachPercent()
+
         
     def printTimeResult(self):
         eachTime = self.iframe_content.findAll("td",class_="datetime2")
@@ -86,9 +87,15 @@ class mbts:
             
     def getEachPercent(self):
         return self.eachPercent
+    
+    def getTimePerDic(self):
+#         print(self.timePerDic.__len__())
+        return self.timePerDic
 
 if __name__ == "__main__":
     bttest = mbts()
     
     bttest.IframeUrlWithCode('000660')
+#     bttest.showEachPercent()
     
+    print(bttest.getTimePerDic())
