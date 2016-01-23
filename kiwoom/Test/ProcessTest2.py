306d38a34c6f904d0dcc733c11d2ae65ea93ed9f
thesky <thesky@DESKTOP-8ECED36>
2016-01-23 12:12:12 +0900

    2

--- kiwoom/Test/ProcessTest2.py
+++ kiwoom/Test/ProcessTest2.py
@@ -1,20 +1,48 @@
 import multiprocessing
 import os
 
-def info(title):
+
+
+class Prt:
     
-    print(title)
-    print('module name:',__name__)
-    print('parent process:',os.getppid())
-    print('process id:',os.getpid())
+    def info(self,title):
+        print(title)
+        print('module name:',__name__)
+        print('parent process:',os.getppid())
+        print('process id:',os.getpid())
     
-def f(name):
-    info('fuction f')
-    print('hello',name)
+    def f(self,start,end,q):
+#         self.info('fuction f')
+
+        print('parent process:',os.getppid())
+        print('process id:',os.getpid())
+        
+        
+        i=start
+        for i in range(start,end):
+            i+=i
+            
+        q.put(i)
     
     
 if __name__ == '__main__':
-    info('main line')
-    p = multiprocessing.Process(target=f,args=('bob',))
-    p.start()
-    p.join()
+    pt = Prt()
+#     pt.info('main line')
+#     p = multiprocessing.Process(target=pt.f('bob'))
+#     p.start()
+#     p.join()
+#     
+#     p = multiprocessing.Process(target=pt.f('bo'))
+#     
+#     
+    q = multiprocessing.Queue()
+    pl=[]
+    q.put(50)
+    
+    for p in range(2):
+        p = multiprocessing.Process(target=pt.f ,args=(1,3,q))
+        pl.append(p)
+        p.start()
+        
+    print(q.get())
+    
