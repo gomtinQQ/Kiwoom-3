8f96808f577e17464a3aa82117b05eee8846c06c
thesky <thesky@DESKTOP-8ECED36>
2016-02-05 14:28:24 +0900

    ewq

--- kiwoom/src/DBMake.py
+++ kiwoom/src/DBMake.py
@@ -6,33 +6,26 @@ import bts
 import time
 import multiprocessing as mp
 from _sqlite3 import OperationalError
+import sys
 
-class dbm(mp.Process):
+class dbm2(mp.Process):
     
     def __init__(self,dbname=""):
-        super(multi, self).__init__()
+        super(dbm2, self).__init__()
         self.conn=""
-        
         if dbname=="":
             self.conn = sqlite3.connect("D:\\OneDrive\\python\\sqlite3\\kosdaq1.db")
         else:
             self.conn = sqlite3.connect(dbname)
             
         self.cursor = self.conn.cursor()
-    
     def getConnection(self,dbname=""):
             
         return self.conn
         
-
-    def setCodelist(self,codelist,tablename,conn):
+    def setCursor(self,conn):
         
-        self.cursor = conn.cursor()
-        
-        for code in codelist:
-            code = str(code)
-            self.cursor.execute("insert into "+tablename+" (StockCode) values ("+code+")")
-        conn.commit()
+        self.cursor=conn.cursor()
         
     def setNamelist(self,namelist,tablename,conn):
         
@@ -55,6 +48,7 @@ class dbm(mp.Process):
         
     def updateCode(self,code,TimePerDict):
         _start = time.time()
+        
         for tp in TimePerDict.keys():
             try:
                 rtime = tp
@@ -62,9 +56,6 @@ class dbm(mp.Process):
                     tp=tp[0:]+'0'+tp[:0]
                 rec=tp[:2]+tp[3:]
                 rec=int(rec)
-#                 print(str(rec))
-#                 print(str(TimePerDict[rtime]))
-#                 print(str(code))
                 self.cursor.execute('update kosdaq set "'+str(rec)+'"="'+str(TimePerDict[rtime])+'" where StockCode='+str(code))
             except OperationalError:
                 continue
@@ -77,10 +68,21 @@ class dbm(mp.Process):
         self.WQueue=WQueue
         
     def run(self):
-        while True:
-            TimePerDict = self.Wqueue.get()
         
-            self.updateCode(code,TimePerDict)
+        _start=time.time()
+        all = len(self.codelist)
+        i=0
+        while True:
+            TimePerDict = self.WQueue.get()
+            if TimePerDict=='END':
+                print('success~ ['+(time.time()-_start)+']')
+                break
+            i+=1
+            for code in TimePerDict:
+                self.updateCode(code,TimePerDict[code])
+            print('=======================================================['+str(i)+'/'+str(all)+']')
+#         print('hi')
+        
         
     def setCodelist(self,codelist):
         self.codelist = codelist
@@ -96,20 +98,24 @@ class multi(mp.Process):
     
     def run(self):
         
+        codeandvalue={}
         while(True):
             print(self.name+" is working")
             code = self.RQueue.get()
             if code == 'END':
                 self.RQueue.put('END')
+                self.WQueue.put('END')
                 break
             self.bts.IframeUrlWithCode(code)
             tpd = self.bts.getTimePerDic()
-            
-            self.WQueue.put(tpd)
+            codeandvalue[code] =tpd 
+            self.WQueue.put(codeandvalue)
                         
 
 if __name__ == '__main__':
 
+    sys.setrecursionlimit(2000) #에러방지위해 뎁스 기본값 세팅
+    
     readedExcel = ExcelMake.ExcelCode(setLayout=False)
     readedExcel.ExcelRead(fileName='D:\\Kiwoo\\ExcelData\\20160127\\20160127_yang_1.xlsx')
     wb = readedExcel.getWorkBook()
@@ -117,11 +123,12 @@ if __name__ == '__main__':
     
     codelist = readedExcel.getCodeList()
     
-    dbm = dbm()
+    WQueue = mp.Queue()
+    dbm = dbm2()
+    
     bts = bts.mbts()
     
     RQueue = mp.Queue()
-    WQueue = mp.Queue()
     for code in codelist:
         RQueue.put(code)
     RQueue.put('END')
@@ -132,9 +139,12 @@ if __name__ == '__main__':
         proc = multi(RQueue,WQueue,bts)
         process.append(proc)
         proc.start()
-     
-    for code in codelist:
          
-    
-    
+#     WQueue.put('END')
+    dbm.setWQueue(WQueue)
+    dbm.setCodelist(codelist)
+    dbm.setCursor(dbm.getConnection())
+    dbm.start()
+    dbm.join()
     dbm.commit()
+
