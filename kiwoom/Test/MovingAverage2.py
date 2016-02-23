1c8277ac490663457114d69ea3bbcbf89e9556cd
youngil <youngil@yang>
2016-02-23 22:27:58 +0900

    10:27

--- kiwoom/Test/MovingAverage2.py
+++ kiwoom/Test/MovingAverage2.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
-
 import numpy as np
 from numpy import convolve
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import pandas as pd
 import sys
 sys.path.append('../src')
 import btsForDaily
 import datetime
 
-def movingaverage(values,window):
+def movingaverage(value,term):
     
-    weights = np.repeat(1.0,window)/window
-    sma = np.convolve(values, weights, 'valid')#mode = valid,same,full
-    # sma = np.correlate(values, weights, 'full')
-#     print(values,weights)
-    return sma
-
+    value = pd.Series(value)
+    yMa=pd.stats.moments.rolling_mean(value,term)
+    return yMa
 
 Data = btsForDaily.daily().getDataFromDaum('126700','2015-9-1')
 
@@ -32,34 +29,21 @@ for time in Data:
 x=[mdates.date2num(i) for i in dt_x]
 
 
-for i in x:
-    print(type(i))
-# x = [1,2,3,4,5,6,7,8,9,10]#x축
-# y = [3,5,2,4,9,1,7,5,9,1] #y축
-# x=np.ndarray(x)
-yMa = movingaverage(y,5)   #day ma
-# print(yMa)
-
-# print(yMa)
+yMa = movingaverage(y,10)   #day ma
+print(yMa)
 
 fig,ax= plt.subplots()
 
-ax.plot_date(x,y,'-')
+ax.plot_date(x[9:],y[9:],'r-')
+
+
+
 date_formatter = mdates.DateFormatter('%Y-%m-%d')
 
 ax.xaxis.set_major_formatter(date_formatter)
 fig.autofmt_xdate()
-plt.plot(x[(len(x)-len(yMa)):],yMa)
-
+# print(len(x))
+plt.plot(x[9:],yMa[9:],'b-')
 plt.show()
 
-# 
-# import pandas.io.data as web
-# import datetime
-# 
-# start = datetime.datetime(2016,1,1)
-# 
-# stock  = web.DataReader("126700.KQ","ff",start)
-#
-# print(stock)
 
